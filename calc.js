a93e928d63c979cb6fec0e26e1fef5b70748e2bf
NotZacM <Zachary.Morris01@hotmail.com>
2024-03-23 11:16:11 -0400

    fixed onclick event problem

--- calc.js
+++ calc.js
@@ -1,23 +1,30 @@
-let num1 = "";
-let num2 = "";
-let operation = "";
-
-function printCalc(){
-   //getting values for all the buttons
-   const buttons = document.querySelectorAll('.btn');
-   buttons.forEach(button =>{
+let num1 = ""
+let num2 = ""
+let operation = ""
+const buttons = document.querySelectorAll('.btn')
+//getting values for all the buttons   
+buttons.forEach(button =>{
     button.addEventListener('click',function(){
-        if(num1 == 0){
-            num1 = this.value;
-            console.log(num1);
-        }else if(num1 !=0 && num2 == 0){
-            operation = this.value;
-            console.log(operation);
+        if(this.value == "="){
+            calcAns()
+        }
+        else if(num1 == ""){
+            num1 = this.value
+            console.log(num1)
+        }
+        else if(num1 != "" && num2 == ""){
+            operation = this.value
+            console.log(operation)
         }
         else{
-            num2 = this.value;
-            console.log(num2);
+            num2 = this.value
+            console.log(num2)
         }
     })
    })
+
+
+function calcAns(){
+    console.log("we are here")
+    console.log(num1,num2,operation)
 }
