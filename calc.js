8f0463fa76d3bbf6728b710d94db4a000ff913cf
NotZacM <Zachary.Morris01@hotmail.com>
2024-03-23 10:35:02 -0400

    fix button shower

--- calc.js
+++ calc.js
@@ -1,14 +1,22 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operation = "";
 
-function printCalc(num1,num2){
+function printCalc(){
    //getting values for all the buttons
    const buttons = document.querySelectorAll('.btn btn-light');
    buttons.forEach(button =>{
     button.addEventListener('click',function(){
-        if(num1 != 0){
-            
+        if(num1 == 0){
+            num1 = this.value;
+            console.log(num1);
+        }else if(num1 !=0 && num2 == 0){
+            operation = this.value;
+            console.log(operation);
+        }
+        else{
+            num2 = this.value;
+            console.log(num2);
         }
     })
    })
