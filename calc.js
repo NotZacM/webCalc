8c6be13fafe4138391db8ec866c6ac62f3fe400f
NotZacM <Zachary.Morris01@hotmail.com>
2024-03-23 09:31:13 -0400

    Initial commit with first few buttons. Still working on javascript section

---
+++ calc.js
@@ -0,0 +1,15 @@
+let num1 = 0;
+let num2 = 0;
+let operation = "";
+
+function printCalc(num1,num2){
+   
+   const buttons = document.querySelectorAll('.btn btn-light');
+   buttons.forEach(button =>{
+    button.addEventListener('click',function(){
+        if(num1 != 0){
+            
+        }
+    })
+   })
+}
