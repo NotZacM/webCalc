6212c864def7afad584d27eae0d4507162857c18
NotZacM <Zachary.Morris01@hotmail.com>
2024-03-24 17:45:18 -0400

    All basic calculations for calculator working now

--- calc.js
+++ calc.js
@@ -1,7 +1,9 @@
 let num1 = ""
 let num2 = ""
 let operation = ""
+let ans = 0
 let buttons = document.querySelectorAll('.btn')
+let calcLine = document.createElement("h2")
 //getting values for all the buttons   
 buttons.forEach(function(button) {
     button.addEventListener('click',function(){
@@ -25,6 +27,22 @@ buttons.forEach(function(button) {
 
 
 function calcAns(){
+    if(operation == "*"){
+        ans = parseFloat(num1 * num2)
+    }
+    else if(operation == "/"){
+        ans = parseFloat(num1 / num2)
+    }
+    else if(operation == "-"){
+        ans = parseFloat(num1 - num2)
+    }
+    else{
+        ans = parseFloat(num1 + num2)
+    }
+    let calc = document.createElement("h2") 
+    calcLine = String(num1 + " " + operation + " " + num2 + " = " + ans)
+    calc.textContent = calcLine
+    document.getElementById("showCalc").appendChild(calc)
     console.log("num1=",num1)
     console.log("num2=",num2)
     console.log("operation=",operation)
