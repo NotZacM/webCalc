91ac92c210a9ebd471eeaa674a7f2d850f981045
NotZacM <Zachary.Morris01@hotmail.com>
2024-03-24 16:00:46 -0400

    got numbers and operands to show up properly

--- calc.js
+++ calc.js
@@ -1,31 +1,30 @@
 let num1 = ""
 let num2 = ""
 let operation = ""
-const buttons = document.querySelectorAll('.btn')
+let buttons = document.querySelectorAll('.btn')
 //getting values for all the buttons   
-buttons.forEach(button =>{
+buttons.forEach(function(button) {
     button.addEventListener('click',function(){
-        if(this.value == "="){
+        if(button.value == "="){
             calcAns()
         }
-        else if(num1 == ""){
-            num1 = this.value
+        else if(button.value != "*" && button.value != "/" && button.value != "+" && button.value != "-" && operation == ""){
+            num1 += button.value
             console.log(num1)
         }
-        else if(num1 != "" && num2 == ""){
-            operation = this.value
-            console.log(operation)
-        }
-        else{
-            num2 = this.value
+        else if(num1 != "" && operation != ""){
+            num2 += button.value
             console.log(num2)
         }
+        else if(num2 == ""){
+            operation += button.value
+            console.log(operation)
+        }  
     })
    })
 
 
 function calcAns(){
-    console.log("we are here")
     console.log("num1=",num1)
     console.log("num2=",num2)
     console.log("operation=",operation)
