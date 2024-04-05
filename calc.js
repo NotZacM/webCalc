95c32bc3d90d18e5c13378def5cd9b0d41c37aa6
Zachary Morris <Zachary.Morris01@hotmail.com>
2024-04-05 19:00:50 -0400

    implemented multiple number calculations TODO: implement bedmas order of operations

--- calc.js
+++ calc.js
@@ -1,56 +1,80 @@
 let num1 = ""
-let num2 = ""
 let operation = ""
 let nums = []
 let operations = []
 let ans = 0
 let buttons = document.querySelectorAll('.btn')
-let calcLine = document.createElement("h2")
+//let calcLine = document.createElement("h2")
+calcLine = ""
+//nums.length != 0 && nums.length % 2 != 0 &&
 //getting values for all the buttons   
 buttons.forEach(function(button) {
     button.addEventListener('click',function(){
         if(button.value == "="){
+            createNum(button.value)
             calcAns()
         }
-        else if(button.value != "*" && button.value != "/" && button.value != "+" && button.value != "-" && operation == ""){
-            num1 += button.value
-            console.log(num1)
+        else{
+            createNum(button.value)
         }
-        else if(num1 != "" && operation != ""){
-            num2 += button.value
-            console.log(num2)
-        }
-        else if(num2 == ""){
-            operation += button.value
-            console.log(operation)
+        if(!(parseFloat(button.value) >= 0 && parseFloat(button.value) <= 9)){
+            operations.push(button.value)
         }  
     })
    })
+
+function createNum(value){
+    if((value == "*" || value == "/" || value == "+" || value == "-" || value == "="))
+    {
+        nums.push(parseFloat(num1))
+        num1 = ""
+        return
+    }
+    num1 += value
+    return
+}
 
 function calcFunc(){
 
 }
 
+function doOperation(op, num1, num2){
+    if(op == "+"){
+        return num1 + num2
+    }else if(op == "-"){
+        return num1 - num2
+    }else if(op == "/"){
+        return num1 / num2
+    }else if(op == "*"){
+        return num1 * num2
+    }
+}
+
 
 function calcAns(){
-    if(operation == "*"){
-        ans = parseFloat(num1 * num2)
+    for(let i = 0; i < nums.length; i++){
+        calcLine += nums[i]
+        calcLine += " "
+        if(i == operations.length){
+            calcLine += "="
+            calcLine += " "
+        }
+        else{
+            calcLine += operations[i]
+            calcLine += " "
+        }
     }
-    else if(operation == "/"){
-        ans = parseFloat(num1 / num2)
+    for(let i = 0; i < operations.length;i++){
+        if(i == 0){
+            ans = doOperation(operations[i],nums[i],nums[i + 1])
+        }else{
+            ans = doOperation(operations[i],ans,nums[i + 1])
+        }
+
     }
-    else if(operation == "-"){
-        ans = parseFloat(num1 - num2)
-    }
-    else{
-        ans = parseFloat(num1 + num2)
-    }
+    calcLine += String(ans)
     let calc = document.createElement("h2") 
-    calcLine = String(num1 + " " + operation + " " + num2 + " = " + ans)
     calc.textContent = calcLine
     document.getElementById("showCalc").appendChild(calc)
-    console.log("num1=",num1)
-    console.log("num2=",num2)
-    console.log("operation=",operation)
 
 }
